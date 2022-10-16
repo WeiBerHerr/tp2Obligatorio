eac4267f877c627c437c0f1da09d6d2fd2b7bda7
Ruben Bl <ruben.h.blanco1@gmail.com>
2022-10-16 16:55:01 -0300

    Creation of double_hash class setting up methods put

---
+++ PackageProyect/double_hash.java
@@ -0,0 +1,30 @@
+package PackageProyect;
+
+import java.util.HashMap;
+
+public class double_hash <K extends Number, V, T> {
+    HashMap<K, V> mp1;
+    HashMap<K, T> mp2;
+
+    public double_hash() {
+        mp1 = new HashMap<>();
+        mp2 = new HashMap<>();
+    }
+    public void addFirst (K key, V value){
+        mp1.put(key, value);
+
+    }
+    public void addSecond(K key, T value){
+        mp2.put(key, value);
+
+    }
+
+
+
+
+
+
+
+
+
+}
